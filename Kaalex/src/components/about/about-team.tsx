13ca48a311a5932137dbd892c5d2b3548da2eebe
ShroukNegeda <sh.ta.3366@gmail.com>
2026-09-16 16:44:19 +0300

    Remove unused file

--- Kaalex/src/components/about/about-team.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useLanguage } from "@/context/language-context";
-import { Team } from "../shared/team";
-
-export function AboutTeam() {
-  const { t } = useLanguage();
-  return <Team title={t.home.team.title} members={t.home.team.members} />;
-}
